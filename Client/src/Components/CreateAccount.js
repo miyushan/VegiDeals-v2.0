51093e5d210b9d0e314d5f7af7a55307a397c006
Nadeesha Maduranga <nadeesham332@gmail.com>
2022-12-20 04:38:55 +0530

    used prettier

--- Client/src/Components/CreateAccount.js
+++ Client/src/Components/CreateAccount.js
@@ -1,41 +1,62 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/CommonStyles.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/CommonStyles.css";
 import { Col, Container, Row, Button } from "react-bootstrap";
-import CreateAccountForm from '../Components/CreateAccountForm';
-import { ReactComponent as Admin } from '../Media/icons/users-cog-solid.svg';
+import CreateAccountForm from "../Components/CreateAccountForm";
+import { ReactComponent as Admin } from "../Media/icons/users-cog-solid.svg";
 
 function CreateAccount() {
+  return (
+    <>
+      <Container id="register" fluid>
+        <Row>
+          <Col
+            xs={12}
+            md={6}
+            className="b_create-left d-flex align-items-center justify-content-center"
+          >
+            <h1 className="b_login-header">
+              Online Vegetable
+              <br />
+              Ordering System
+            </h1>
+            <div className="b_admin_login">
+              <a href="db/login">
+                <Admin className="b_admin-login-icon" height="20px" />
+              </a>
+            </div>
+          </Col>
+          <Col
+            xs={12}
+            md={6}
+            className="b_create-right d-flex align-items-center justify-content-center"
+          >
+            <Row className="b_login_row">
+              <Col className="d-block justify-content-center">
+                <div className="b_login-title b_login-title-1 b_add-curser">
+                  Welcome to VegiDeals
+                </div>
+                <div className="b_login-title b_login-title-2 b_add-curser">
+                  We make you smart
+                </div>
+                <CreateAccountForm />
+              </Col>
+            </Row>
 
-    return (
-        <>
-            <Container id="register" fluid>
-                <Row>
-                    <Col xs={12} md={6} className="b_create-left d-flex align-items-center justify-content-center">
-                        <h1 className="b_login-header">Online Vegetable<br />Ordering System</h1>
-                        <div className="b_admin_login">
-                            <a href="db/login"><Admin className="b_admin-login-icon" height="20px" /></a>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={6} className="b_create-right d-flex align-items-center justify-content-center">
-                        
-                        <Row className="b_login_row">
-                            <Col className="d-block justify-content-center">
-                                <div className="b_login-title b_login-title-1 b_add-curser">Welcome to VegiDeals</div>
-                                <div className="b_login-title b_login-title-2 b_add-curser">We make you smart</div>
-                                <CreateAccountForm />
-                            </Col>
-                        </Row>
-
-                        <div className="b_create-account">
-                            <Button href="login" className="b_create-account-btn" type="submit" variant="success">Log In</Button>
-                        </div>
-                    </Col>
-
-                </Row>
-            </Container>
-
-        </>
-    );
+            <div className="b_create-account">
+              <Button
+                href="login"
+                className="b_create-account-btn"
+                type="submit"
+                variant="success"
+              >
+                Log In
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default CreateAccount;
--- Client/src/Pages/Loginpage.js
+++ Client/src/Pages/Loginpage.js
@@ -1,65 +1,62 @@
-import React, { Component } from 'react';
-import Login from '../Components/Login';
-import HashLoader from 'react-spinners/HashLoader';
-import './Loginpage.css'
+import React, { Component } from "react";
+import Login from "../Components/Login";
+import HashLoader from "react-spinners/HashLoader";
+import "./Loginpage.css";
 
 class Loginpage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinner: true,
+      show: false,
+      isNeedGuide: false,
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            spinner: true,
-            show: false,
-            isNeedGuide: false
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        spinner: false,
+        show: true,
+      });
+    }, 2700);
+    setTimeout(() => {
+      try {
+        //get data in the session
+        let data = localStorage.getItem("newUser");
+        data = JSON.parse(data);
+        if (data.isCreatedAcc) {
+          this.setState({
+            isNeedGuide: true,
+          });
         }
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
+        // console.log("Successful Login!");
+      } catch (err) {}
+    }, 3000);
+  }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                spinner: false,
-                show: true
-            })
-        }, 2700)
-        setTimeout(() => {
-            try {
-                //get data in the session
-                let data = localStorage.getItem('newUser');
-                data = JSON.parse(data);
-                if(data.isCreatedAcc){
-                    
-                    this.setState({
-                        isNeedGuide: true,
-                      
-                    })
-                }
-                // console.log("Successful Login!");
-         
-            }
-            catch (err) {
-                
-            }
- 
-        }, 3000)
-    }
-
-    render() {
-        return (
-            <>
-                {this.state.spinner ?
-                    <div className="cliploader-div d-flex justify-content-center align-items-center">
-                        <HashLoader className="cliploader" size={80} color={"0f0"} />
-                    </div>
-                    : null}
-                <div style={{ display: this.state.show ? "block" : "none" }}>
-                    <Login />
-                </div>
-                {this.state.isNeedGuide ? window.confirm("Now you can LogIn to the system using your mobile number and password.\n(If you want to exit from User Guide press 'Cancel')")?null: localStorage.removeItem('newUser'): null}
-            </>
-        );
-    }
-
+  render() {
+    return (
+      <>
+        {this.state.spinner ? (
+          <div className="cliploader-div d-flex justify-content-center align-items-center">
+            <HashLoader className="cliploader" size={80} color={"0f0"} />
+          </div>
+        ) : null}
+        <div style={{ display: this.state.show ? "block" : "none" }}>
+          <Login />
+        </div>
+        {this.state.isNeedGuide
+          ? window.confirm(
+              "Now you can LogIn to the system using your mobile number and password.\n(If you want to exit from User Guide press 'Cancel')"
+            )
+            ? null
+            : localStorage.removeItem("newUser")
+          : null}
+      </>
+    );
+  }
 }
 
 export default Loginpage;
